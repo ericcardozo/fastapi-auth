2446b75c902694ceab136368774458d0169abc69
ericcardozo <eric.m.cardozo@mi.unc.edu.ar>
2023-12-19 05:13:39 -0300

    issing files

--- infrastructure/migrations/002_users_profiles.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS users_profiles(
-    id SERIAL PRIMARY KEY,
-    first_name VARCHAR(50) NOT NULL,
-    last_name VARCHAR(50) NOT NULL,
-    birthdate DATE,
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    FOREIGN KEY (id) REFERENCES users_credentials(id)
-);
